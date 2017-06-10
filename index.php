48ead08c43a4be3080a456d73bdaae0f397e58de
Pierre Chavez <PierreChavez@users.noreply.github.com>
2017-06-10 17:44:45 -0500

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ and open the template in the editor.
         <?php include("codigos/header.php")?>
         <?php include("codigos/nav.php")?>
 
-        <main>
+        <section id='contenedor'>
             <section class="uno">
                 <div class="principal">
                     <h2><i>"Consigue los mejores productos al menor costo"</i></h2>
@@ -28,29 +28,28 @@ and open the template in the editor.
                     <img src="" alt=""/>
                 </div>
             </section>
-        </main>
-        
-        <div id="form-registro">
-            <!---controlRegistro.php?op=1&ca=r-->
+        </section>
+        <aside id='barraLateral'>
+            <h3>Registrate</h3>
             <form method="post" action="" onsubmit="return validacionFormularioRegistro()">
                 <label for="nombreUsuario">Nombre de usuario:</label><br/>
-                <input type="text" class="combo" name="nombreUsuario" id="campoUsuario" placeholder="Usuario"/><br/>
+                <input type="text" name="nombreUsuario" id="campoUsuario"/><br/>
                 <label for="correo"> E-mail:</label><br/>
-                <input type="email" class="combo" name="correo" id="campoEmail" placeholder="Email"><br/>
+                <input type="email" name="correo" id="campoEmail"><br/>
                 <label for="claveUsuario">Contraseña de usuario:</label><br/>
-                <input type="password" class="combo" name="claveUsuario" id="campoContrasenia" placeholder="Contraseña"><br/>
+                <input type="password" name="claveUsuario" id="campoContrasenia"><br/>
                 <label for="claveUsuario">Confirmar contraseña:</label><br/>
-                <input type="password" class="combo" name="claveUsuario" id="campoContrasenia2" placeholder="Contraseña"><br/>
+                <input type="password" name="claveUsuario" id="campoContrasenia2"><br/>
                 <label for="radio">Genero:</label><br/>
                 <input type="radio" name="genero" value="hombre"><label>Masculino</label><br/>
                 <input type="radio" name="genero" value="mujer"><label>Femenino</label><br/>
                 <input type="radio" name="genero" value="otro"><label>Otro</label><br/>
                 <label for="fechaNacimiento">Fecha de Nacimiento:</label><br/>
-                <input type="date" name="fechaNacimiento" min="1930-01-01" max="2002-12-31" id="campoFechaNac" class="combo"><br/>
+                <input type="date" name="fechaNacimiento" min="1930-01-01" max="2002-12-31" id="campoFechaNac"><br/>
                 <input type="submit" value="Registrar" name="botonRegistrar">
                 <input type="reset" value="Limpiar">
             </form>
-        </div>
+        </aside>
         <script src="js/funciones.js" type="text/javascript">
             
         </script>
