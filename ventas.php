b371f28a3e2220afe61298cf99cfacba82d37baa
lupera13k <kevinlupera1997@outlook.com>
2017-06-10 20:10:46 -0500

    Create ventas.php

--- ventas.php
+++ ventas.php
@@ -12,9 +12,54 @@ and open the template in the editor.
     <body>
         <?php include("codigos/header.php")?>
         <?php include("codigos/nav.php")?>
-        <?php
-        // put your code hereaasdasdsad
-        ?>
+        <!-- Product grid -->
+        <div class="contenedor">
+            <div class="contenedorProductos">
+                <table border="1">
+                    <tr>
+                      <td>
+                          <div class="producto">
+                            <img src="/imagenes/jeans1.jpg" style="width:100%">
+                            <p>Producto1<br><b>$24.99</b></p>
+                          </div>
+                      </td>
+                      <td>
+                          <div class="producto">
+                            <img src="/imagenes/jeans1.jpg" style="width:100%">
+                            <p>Producto2<br><b>$24.99</b></p>
+                          </div>
+                      </td>
+                      <td>
+                          <div class="producto">
+                            <img src="/imagenes/jeans1.jpg" style="width:100%">
+                            <p>Producto3<br><b>$24.99</b></p>
+                          </div>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                          <div class="producto">
+                            <img src="/imagenes/jeans1.jpg" style="width:100%">
+                            <p>Producto4<br><b>$24.99</b></p>
+                          </div>
+                      </td>
+                      <td>
+                          <div class="producto">
+                            <img src="/imagenes/jeans1.jpg" style="width:100%">
+                            <p>Producto5<br><b>$24.99</b></p>
+                          </div>
+                      </td>
+                      <td>
+                          <div class="producto">
+                            <img src="/imagenes/jeans1.jpg" style="width:100%">
+                            <p>Producto6<br><b>$24.99</b></p>
+                          </div>
+                      </td>
+                    </tr>
+
+                </table>
+            </div>
+        </div>
         <?php include("codigos/footer.php")?>
     </body>
 </html>
