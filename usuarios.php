97ad3a9ea7ee8b4cebfe4ba955058304928714c4
lupera13k <kevinlupera1997@outlook.com>
2017-06-11 23:29:36 -0500

    Merge master into NN
    Conflicts:
    	codigos/footer.php
    	login.php
    	loginController.php

--- usuarios.php
+++ usuarios.php
@@ -8,11 +8,119 @@ and open the template in the editor.
     <head>
         <?php include("codigos/meta.php")?>
         <title>ALLBUY-USUARIOS</title>
+        <style>
+            .contenedorUsuario{
+                display: block;
+                width: 90%;
+                padding: 20px;
+                margin-top: 10px;
+                margin: auto;
+                
+            }
+            .usuario{
+                margin-top: 20px;
+                border-bottom: 2px solid gray;
+            }
+            .fotoUsuario,.infoUsuario,.opciones{
+                display: inline-block;
+                vertical-align: top;
+            }
+            .infoUsuario{
+                
+                width: 25%;
+                
+            }
+            .infoUsuario span{
+                margin-left: 20px;
+                font-weight: bold;
+                padding: 10px;
+                
+            }
+            .opciones{
+                width: 40%;
+            }
+            .opciones a{
+                color: white;
+                text-decoration: none;
+                padding: 7px;
+                background: #24292e;
+                font-weight: bold;
+                border-radius: 3px;
+            }
+        </style>
     </head>
     <body>
         <?php include("codigos/header.php")?>
         <?php include("codigos/nav.php")?>
-        <div class="contenedor"></div>
+        <div class="contenedor">
+            <div class="contenedorUsuario">
+            <div class="usuario" id="usuario1">
+                <a href="#" class="fotoUsuario"><img src="imagenes/user-image.png" alt="User" style="height:100px;"></a>
+                <div class="infoUsuario">
+                    <span id="nombreUsuario1">Juan Sanchez</span><br/>
+                    <span id="nombreUsuario1">0931551555</span>
+                </div>
+                <div class="opciones">
+                    <a href="#" onclick=""><i class="fa fa-users" aria-hidden="true"></i>&nbsp;Perfil</a>
+                    <a href="#" onclick=""><i class="fa fa-print" aria-hidden="true"></i>&nbsp;Compras</a>
+                    <a href="#" onclick=""><i class="fa fa-times" aria-hidden="true"></i>&nbsp;Bloquear</a>
+                    <a href="#" onclick=""><i class="fa fa-cart-plus" aria-hidden="true"></i>&nbsp;Cesta</a>
+                </div>
+            </div>
+            <div class="usuario" id="usuario2">
+                <a href="#" class="fotoUsuario"><img src="imagenes/user-image.png" alt="User" style="height:100px;"></a>
+                <div class="infoUsuario">
+                    <span id="nombreUsuario2">Juan2 Sanchez</span><br/>
+                    <span id="nombreUsuario2">0931551555</span>
+                </div>
+                <div class="opciones">
+                    <a href="#" onclick=""><i class="fa fa-users" aria-hidden="true"></i>&nbsp;Perfil</a>
+                    <a href="#" onclick=""><i class="fa fa-print" aria-hidden="true"></i>&nbsp;Compras</a>
+                    <a href="#" onclick=""><i class="fa fa-times" aria-hidden="true"></i>&nbsp;Bloquear</a>
+                    <a href="#" onclick=""><i class="fa fa-cart-plus" aria-hidden="true"></i>&nbsp;Cesta</a>
+                </div>
+            </div>
+            <div class="usuario" id="usuario3">
+                <a href="#" class="fotoUsuario"><img src="imagenes/user-image.png" alt="User" style="height:100px;"></a>
+                <div class="infoUsuario">
+                    <span id="nombreUsuario3">Juan3 Sanchez</span><br/>
+                    <span id="nombreUsuario3">0931551555</span>
+                </div>
+                <div class="opciones">
+                    <a href="#" onclick=""><i class="fa fa-users" aria-hidden="true"></i>&nbsp;Perfil</a>
+                    <a href="#" onclick=""><i class="fa fa-print" aria-hidden="true"></i>&nbsp;Compras</a>
+                    <a href="#" onclick=""><i class="fa fa-times" aria-hidden="true"></i>&nbsp;Bloquear</a>
+                    <a href="#" onclick=""><i class="fa fa-cart-plus" aria-hidden="true"></i>&nbsp;Cesta</a>
+                </div>
+            </div>
+            <div class="usuario" id="usuario4">
+                <a href="#" class="fotoUsuario"><img src="imagenes/user-image.png" alt="User" style="height:100px;"></a>
+                <div class="infoUsuario">
+                    <span id="nombreUsuario4">Juan4 Sanchez</span><br/>
+                    <span id="nombreUsuario4">0931551555</span>
+                </div>
+                <div class="opciones">
+                    <a href="#" onclick=""><i class="fa fa-users" aria-hidden="true"></i>&nbsp;Perfil</a>
+                    <a href="#" onclick=""><i class="fa fa-print" aria-hidden="true"></i>&nbsp;Compras</a>
+                    <a href="#" onclick=""><i class="fa fa-times" aria-hidden="true"></i>&nbsp;Bloquear</a>
+                    <a href="#" onclick=""><i class="fa fa-cart-plus" aria-hidden="true"></i>&nbsp;Cesta</a>
+                </div>
+            </div>
+            <div class="usuario" id="usuario5">
+                <a href="#" class="fotoUsuario"><img src="imagenes/user-image.png" alt="User" style="height:100px;"></a>
+                <div class="infoUsuario">
+                    <span id="nombreUsuario5">Juan5 Sanchez</span><br/>
+                    <span id="nombreUsuario15">0931551555</span>
+                </div>
+                <div class="opciones">
+                    <a href="#" onclick=""><i class="fa fa-users" aria-hidden="true"></i>&nbsp;Perfil</a>
+                    <a href="#" onclick=""><i class="fa fa-print" aria-hidden="true"></i>&nbsp;Compras</a>
+                    <a href="#" onclick=""><i class="fa fa-times" aria-hidden="true"></i>&nbsp;Bloquear</a>
+                    <a href="#" onclick=""><i class="fa fa-cart-plus" aria-hidden="true"></i>&nbsp;Cesta</a>
+                </div>
+            </div>
+            </div>
+        </div>
         <?php include("codigos/footer.php")?>
     </body>
 </html>
